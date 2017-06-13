d9010aa04a38fcd4ba302e28976ce7b52119a19b
YentlWils <yentl_wils@hotmail.com>
2017-06-13 10:47:05 +0200

    add remark

--- footer/instagram-widget.php
+++ footer/instagram-widget.php
@@ -9,17 +9,11 @@
 //https://www.instagram.com/oauth/authorize/?client_id=9bbb18416af04e59afa19babf0f6df50&amp;redirect_uri=http://medica.be&amp;response_type=token&amp;scope=public_content
 //53977759
 
+/** TODO make token and username configurable*/
+
 $access_token = '53977759.9bbb184.44b07fe5225f473888651e041dbc3939';
 $username = 'yentl_wils';
 $user_search = rudr_instagram_api_curl_connect("https://api.instagram.com/v1/users/search?q=" . $username . "&access_token=" . $access_token);
-// $user_search is an array of objects of all found users
-// we need only the object of the most relevant user - $user_search->data[0]
-// $user_search->data[0]->id - User ID
-// $user_search->data[0]->first_name - User First name
-// $user_search->data[0]->last_name - User Last name
-// $user_search->data[0]->profile_picture - User Profile Picture URL
-// $user_search->data[0]->username - Username
-
 $user_id = $user_search->data[0]->id; // or use string 'self' to get your own media
 
 $return = rudr_instagram_api_curl_connect("https://api.instagram.com/v1/users/" . $user_id . "/media/recent?access_token=" . $access_token);
